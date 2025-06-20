adc9b5f4fd0c694fe99af2ab20b0b05da984f877
enviafotos <enviafotos37@gmail.com>
2025-06-20 02:07:07 -0500

    Add files via upload

--- historial.py
+++ historial.py
@@ -1,16 +1,12 @@
 import csv
 import os
 from datetime import datetime
-RUTA_HISTORIAL = "historial_envios/historial_envios.csv"
 def guardar_en_historial(dni, nombre):
-   try:
-       os.makedirs("historial_envios", exist_ok=True)  # Crea la carpeta si no existe
-       archivo_existe = os.path.exists(RUTA_HISTORIAL)
-       with open(RUTA_HISTORIAL, mode='a', newline='', encoding='utf-8') as archivo_csv:
-           writer = csv.writer(archivo_csv)
-           if not archivo_existe:
-               writer.writerow(["DNI", "Nombre", "Fecha y Hora"])
-           writer.writerow([dni, nombre, datetime.now().strftime("%Y-%m-%d %H:%M:%S")])
-       print("✔ Historial guardado correctamente.")
-   except Exception as e:
-       print("❌ Error al guardar en historial:", e)
+   ruta = "historial_envios/historial.csv"
+   os.makedirs("historial_envios", exist_ok=True)
+   existe = os.path.exists(ruta)
+   with open(ruta, mode='a', newline='', encoding='utf-8') as archivo:
+       escritor = csv.writer(archivo)
+       if not existe:
+           escritor.writerow(["DNI", "Nombre", "Fecha y Hora"])
+       escritor.writerow([dni, nombre, datetime.now().strftime("%Y-%m-%d %H:%M:%S")])
