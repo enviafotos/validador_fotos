99778b763a2637b98785fe9e90493c86d0203690
enviafotos <enviafotos37@gmail.com>
2025-06-20 01:47:02 -0500

    Update historial.py

--- historial.py
+++ historial.py
@@ -8,5 +8,5 @@ def guardar_en_historial(dni, nombre):
    with open(RUTA_HISTORIAL, mode='a', newline='', encoding='utf-8') as archivo_csv:
        writer = csv.writer(archivo_csv)
        if not archivo_existe:
-           writer.writerow(["DNI", "Nombre", "Fecha y Hora"])
+           writer.writerow(["DNI","Nombre","Fecha y Hora"])
        writer.writerow([dni, nombre, datetime.now().strftime("%Y-%m-%d %H:%M:%S")])
